2c7406e0fb97150bf123cdd7e19f89ac7a2c4d74
Edith Hdz <ehngit@gmail.com>
2020-03-07 01:20:15 -0600

    Creando rutas en Angular

--- src/app/components/product.components.ts
+++ src/app/components/product.components.ts
@@ -14,32 +14,35 @@ import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product',
-  templateUrl: './product.component.html'
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements /*OnChanges,*/ OnInit, DoCheck, OnDestroy {
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
 
   @Input() product: Product;
   @Output() productClicked: EventEmitter<any> = new EventEmitter();
 
+  today = new Date();
+
   constructor() {
-    console.log('1.Contructor');
+    console.log('1. constructor');
   }
 
-  /*ngOnChanges(changes: SimpleChanges) {
-     console.log('2. ngOnChanges');
-     console.log(changes);
-   }*/
+  // ngOnChanges(changes: SimpleChanges) {
+  //   console.log('2. ngOnChanges');
+  //   console.log(changes);
+  // }
 
   ngOnInit() {
     console.log('3. ngOnInit');
   }
 
-  onDoCheck() {
-    console.log('4.onDoCheck');
+  ngDoCheck() {
+    console.log('4. ngDoCheck');
   }
 
   ngOnDestroy() {
-    console.log('5.ngOnDestroy');
+    console.log('5. ngOnDestroy');
   }
 
   addCart() {
